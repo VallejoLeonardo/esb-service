d09c5b116eaf092a6f2b4edeaace2d22e69ac36a
VallejoLeonardo <leonardo_3041230313@utd.edu.mx>
2025-03-28 09:53:16 -0600

    commit init

--- src/main/java/utd/ti/esb_service/config/WebClientConfig.java
+++ src/main/java/utd/ti/esb_service/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package utd.ti.esb_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,6 +8,36 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     
+    @Value("${CLIENTES_API_URL:http://localhost:7000}")
+    private String clientesApiUrl;
+
+    @Value("${USERS_API_URL:http://localhost:5000}")
+    private String usersApiUrl;
+
+    @Value("${EMAIL_API_URL:http://localhost:8000}")
+    private String emailApiUrl;
+    
+    @Bean
+    public WebClient usersWebClient() {
+        return WebClient.builder()
+                .baseUrl(usersApiUrl)
+                .build();
+    }
+    
+    @Bean
+    public WebClient clientesWebClient() {
+        return WebClient.builder()
+                .baseUrl(clientesApiUrl)
+                .build();
+    }
+    
+    @Bean
+    public WebClient emailWebClient() {
+        return WebClient.builder()
+                .baseUrl(emailApiUrl)
+                .build();
+    }
+
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
